c6f7483506e0a37c95fe4d965ff43acb361d8cbe
PrabhnoorSingh-123 <60430048+PrabhnoorSingh-123@users.noreply.github.com>
2020-10-18 14:01:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var hypnoticBall, database;
+var playerCount,form,player,game;
+var gameState = 0;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+ game = new Game()
+ game.getState() 
+ game.start()
+ 
+  
+}
+
+function draw(){
+ 
+  
+ 
+}
